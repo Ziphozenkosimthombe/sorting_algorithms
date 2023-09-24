168f9f7e5ac83a4c2747697646085546171ad89a
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2023-09-24 05:39:53 -0700

    sorting_algorithms

---
+++ 100-shell_sort.c
@@ -0,0 +1,46 @@
+#include "sort.h"
+
+/**
+ * swap - the two integers will be swap
+ * in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
+ */
+void swap(int *i, int *y)
+{
+	int temp;
+
+	temp = *i;
+	*i = *y;
+	*y = temp;
+}
+
+/**
+ * shell_sort - Sort an array of integers in ascending order.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ */
+void shell_sort(int *array, size_t size)
+{
+	size_t gap, a, j;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (gap = 1; gap < (size / 3);)
+		gap = gap * 3 + 1;
+
+	for (; gap >= 1; gap /= 3)
+	{
+		for (a = gap; a < size; a++)
+		{
+			j = a;
+			while (j >= gap && array[j - gap] > array[j])
+			{
+				swap(array + j, array + (j - gap));
+				j -= gap;
+			}
+		}
+		print_array(array, size);
+	}
+}
